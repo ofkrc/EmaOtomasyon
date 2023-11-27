2a24fdbe35722eef28f815700c1deee692366455
ofkrc <ofkaraca44@gmail.com>
2023-11-27 18:47:16 +0300

    Add project files.

---
+++ EmaOtomasyon/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EmaOtomasyon.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
