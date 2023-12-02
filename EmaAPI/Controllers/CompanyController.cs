88c1e62be2029c6572cff915de5bec158fe49054
ofkrc <ofkaraca44@gmail.com>
2023-12-02 22:10:14 +0300

    Company bitti.

--- EmaAPI/Controllers/CompanyController.cs
+++ EmaAPI/Controllers/CompanyController.cs
@@ -1,5 +1,8 @@
-﻿using EmaAPI.Services;
+﻿using EmaAPI.Models.Request.Item;
+using EmaAPI.Models;
+using EmaAPI.Services;
 using Microsoft.AspNetCore.Mvc;
+using EmaAPI.Models.Request.Company;
 
 namespace EmaAPI.Controllers
 {
@@ -12,6 +15,73 @@ namespace EmaAPI.Controllers
 		public CompanyController(ICompanyService companyService)
 		{
 			_companyService = companyService;
+		}
+
+		[HttpPost("Insert")]
+		public ActionResult<Company> Insert([FromBody] CompanyRequestModel request)
+		{
+			var newItem = _companyService.Insert(request);
+
+			return CreatedAtAction(nameof(Insert), new { id = newItem.RecordId }, newItem);
+		}
+
+		[HttpPut("Update")]
+		public IActionResult Update(int companyId, [FromBody] CompanyRequestModel request)
+		{
+			var updatedCompany = _companyService.Update(companyId, request);
+			if (updatedCompany != null)
+			{
+				return Ok(updatedCompany);
+			}
+			return NotFound();
+		}
+
+		[HttpGet("GetAllSearch")]
+		public IActionResult Search()
+		{
+			try
+			{
+				var companies = _companyService.Search();
+				return Ok(companies);
+			}
+			catch (Exception ex)
+			{
+				// Diğer olası hataları ele alabilirsiniz.
+				return StatusCode(500, "Bir hata oluştu.");
+			}
+		}
+
+		[HttpGet("SearchCompanies")]
+		public IActionResult SearchCompanies(string searchTerm)
+		{
+			try
+			{
+				var companies = _companyService.SearchCompanies(searchTerm);
+				return Ok(companies);
+			}
+			catch (Exception ex)
+			{
+				return StatusCode(500, "Bir hata oluştu.");
+			}
+		}
+
+		[HttpDelete("{id}")]
+		public IActionResult DeleteCompanies(int id)
+		{
+			try
+			{
+				_companyService.DeleteCompanies(id);
+				return Ok("Ürün başarıyla silindi.");
+			}
+			catch (InvalidOperationException ex)
+			{
+				return NotFound(ex.Message);
+			}
+			catch (Exception ex)
+			{
+				// Diğer olası hataları ele alabilirsiniz.
+				return StatusCode(500, "Bir hata oluştu.");
+			}
 		}
 	}
 }
--- EmaAPI/Services/CompanyService.cs
+++ EmaAPI/Services/CompanyService.cs
@@ -1,11 +1,18 @@
 ﻿using EmaAPI.Context;
+using EmaAPI.Models.Request.Item;
+using EmaAPI.Models;
+using EmaAPI.Models.Request.Company;
 
 namespace EmaAPI.Services
 {
 
 	public interface ICompanyService
 	{
-
+		Company Insert(CompanyRequestModel request);
+		Company Update(int companyId, CompanyRequestModel request);
+		IEnumerable<Company> Search();
+		IEnumerable<Company> SearchCompanies(string searchTerm);
+		void DeleteCompanies(int companiesId);
 	}
 	public class CompanyService : ICompanyService
 	{
@@ -13,6 +20,99 @@ namespace EmaAPI.Services
 		public CompanyService(EmaDbContext dbContext)
 		{
 			_dbContext = dbContext;
+		}
+
+		public Company Insert(CompanyRequestModel request)
+		{
+			var newCompany = new Company
+			{
+				CompanyName = request.CompanyName,
+				Address = request.Address,
+				Email = request.Email,
+				TaxNo = request.TaxNo,
+				TaxOffice = request.TaxOffice,
+				Website = request.Website,
+				PhoneNumber = request.PhoneNumber,
+				Status = request.Status,
+				Deleted = request.Deleted,
+				UserId = request.UserId			
+			};
+
+			_dbContext.Companies.Add(newCompany);
+			_dbContext.SaveChanges();
+
+			return newCompany;
+		}
+
+		public Company Update(int companyId, CompanyRequestModel request)
+		{
+			var existingCompany = _dbContext.Companies.Find(companyId);
+
+			if (existingCompany == null)
+			{
+
+				throw new InvalidOperationException("Belirtilen ID'ye sahip şirket bulunamadı.");
+			}
+
+			existingCompany.RecordId = request.RecordId;
+			existingCompany.CompanyName = request.CompanyName;
+			existingCompany.Address = request.Address;
+			existingCompany.Email = request.Email;
+			existingCompany.TaxNo = request.TaxNo; 
+			existingCompany.TaxOffice = request.TaxOffice; 
+			existingCompany.Website = request.Website;
+			existingCompany.PhoneNumber = request.PhoneNumber;
+			existingCompany.Status = request.Status;
+			existingCompany.UserId = request.UserId;
+			existingCompany.Deleted = request.Deleted;
+
+			_dbContext.SaveChanges();
+
+			return existingCompany;
+		}
+
+		public IEnumerable<Company> Search()
+		{
+			var query = _dbContext.Companies.AsQueryable();
+			query = query.Where(i => !i.Deleted.HasValue || (i.Deleted.HasValue && i.Deleted.Value == false));
+
+			return query.ToList();
+		}
+
+		public IEnumerable<Company> SearchCompanies(string searchTerm)
+		{
+			var query = _dbContext.Companies.AsQueryable();
+
+			if (!string.IsNullOrEmpty(searchTerm))
+			{
+				query = query.Where(i =>
+					i.CompanyName.Contains(searchTerm) ||
+					i.Address.Contains(searchTerm) ||
+					i.PhoneNumber.Contains(searchTerm) ||
+					i.TaxNo.Contains(searchTerm) ||
+					i.TaxOffice.Contains(searchTerm) ||
+					i.Email.Contains(searchTerm));
+
+			}
+
+			query = query.Where(i => !i.Deleted.HasValue || (i.Deleted.HasValue && i.Deleted.Value == false));
+
+			return query.ToList();
+		}
+
+		public void DeleteCompanies(int companiesId)
+		{
+			var companyToDelete = _dbContext.Companies.FirstOrDefault(i => i.RecordId == companiesId);
+
+			if (companyToDelete != null)
+			{
+				companyToDelete.Deleted = true;
+				_dbContext.SaveChanges();
+			}
+			else
+			{
+				throw new InvalidOperationException("Belirtilen ID'ye sahip şirket bulunamadı.");
+			}
 		}
 	}
 }
